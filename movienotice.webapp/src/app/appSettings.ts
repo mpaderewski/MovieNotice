2420ac40f551b757dbabcf91b2e0b27e57737ea1
mpaderewski <paderewski.mateusz@gmail.com>
2023-02-18 22:37:40 +0100

    Dodanie pustego komponentu od stronu User, dodanie endpointa do pobierania inf o uzytowniku

--- movienotice.webapp/src/app/appSettings.ts
+++ movienotice.webapp/src/app/appSettings.ts
@@ -1,5 +1,5 @@
 export namespace AppSettings {
-    export const _API_URL = 'https://localhost:44366/api/'
+    export const _API_URL = 'http://api.movienotice.site/api/'
 
     export const _SRC_APP_PATH = 'src/app/'
     export const _SERVICE_PATH = _SRC_APP_PATH + '_services/'
